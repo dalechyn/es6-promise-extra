676d2ae837d3066d9823a557a12ab785539417cc
h0tw4t3r <hotwater438@tutanota.com>
2019-04-09 18:00:49 +0300

    finished

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const toMix = {
           pr => Promise.resolve(pr).then(
             resolve,
             err => { 
-              if(rejected !== prs.length - 1)
-                rejected++
-              else 
+              if(rejected === prs.length - 1)
                 Promise.reject(err).catch(reject) 
             })
         )
@@ -25,18 +23,54 @@ const toMix = {
       prs.forEach(
         pr => Promise.resolve(pr).then(
           res => {
-            if(resolved + rejected !== prs.length - 1)
-              resolved++
-            else
+            if(resolved + rejected === prs.length - 1)
               Promise.resolve(res).then(resolve)
+            else resolved++
           },
           err => { 
-            if(rejected !== prs.length - 1)
-              rejected++
-            else {
+            if(rejected === prs.length - 1)
               Promise.reject(err).catch(reject) 
+            else rejected++
             }
-          })
+          )
+      )
+    }),
+  any:
+    prs => new Promise((resolve, reject) => {
+      const arr = new Array(prs.length)
+      let resolved = 0
+      let rejected = 0
+      prs.forEach(
+        (pr, i) => Promise.resolve(pr).then(
+          res => { 
+            arr[i] = res 
+            if(resolved + rejected === prs.length - 1)
+              Promise.resolve(arr).then(resolve)
+            else resolved++
+          },
+          err => { 
+            arr[i] = err 
+            if(resolved + rejected === prs.length - 1)
+              Promise.resolve(arr).then(resolve)
+            else rejected++
+          }
+        )
+      )
+    }),
+  none:
+    prs => new Promise((resolve, reject) => {
+      const arr = new Array(prs.length)
+      let rejected = 0
+      prs.forEach(
+        (pr, i) => Promise.resolve(pr).then(
+          res => Promise.reject(res).catch(reject),
+          err => {
+            arr[i] = err
+            if(rejected === prs.length - 1)
+              Promise.resolve(arr).then(resolve)
+            else rejected++
+          }
+        )
       )
     })
 }
